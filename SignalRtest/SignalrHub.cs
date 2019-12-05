cc83fcbbcb48915874767cc72a2b30eeb0dfd625
Vas Alex <daiver777@mail.ru>
2019-12-06 00:23:31 +0300

    Добавьте файлы проекта.

---
+++ SignalRtest/SignalrHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRtest
+{
+    public class SignalrHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            string connId = Context.ConnectionId;
+            string userId = Context.UserIdentifier;
+            await Clients.All.SendAsync("Send", message + " - " + connId + " - " + userId, userName);
+            //await Clients.Client(Context.)
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await this.Clients.All.SendAsync("Send", "У нас новое соединение Id - " + Context.ConnectionId, "Сервер мессангер");
+
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+           await this.Clients.All.SendAsync("Send", Context.ConnectionId + " отсоединился", "Сервер мессангер");
+            
+        }
+    }
+
+}
